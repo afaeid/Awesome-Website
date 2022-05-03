9f9524f6f1bff276e7734de3663f2bf153acf480
liksonbrother082527@gmail.com <liksonbrother082527@gmail.com>
2022-05-03 21:56:13 +0600

    ok

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
+
+
+/* ===== Declaring Variables for the Function Below ===== */
 var menuIcon = document.querySelector('.menu-icon');
 var sidebar = document.querySelector('.mobile-sideber')
-
+var mainContent = document.querySelector('.content');
+var footer = document.querySelector('footer')
+/*===== Sidebar Open Function ===== */
 menuIcon.onclick =  ()=>{
-  sidebar.classList.toggle('show');
+  sidebar.classList.toggle('openSidebar');
+  if(sidebar.classList.contains('openSidebar')){
+    mainContent.onclick = ()=>{
+      sidebar.classList.remove('openSidebar');
+    }
+    footer.onclick = ()=>{
+      sidebar.classList.remove('openSidebar');
+    }
+  }
+}
+
+
+/* ===== Navbar Animation on Scroll ===== */
+var navbar = document.querySelector('nav')
+window.onscroll = ()=>{
+  navbar.classList.toggle('animNav', scrollY > 0);
 }
